61cff0b37b9dcc0bffbd8f2f76f3eb4ffe343f35
wawanneutron <wawanneutron1331@gmail.com>
2022-07-13 15:10:07 +0700

    configurasi JWT in model Customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,8 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class Customer extends Model
+class Customer extends Authenticatable implements JWTSubject
 {
   use HasFactory;
 
@@ -18,6 +20,37 @@ class Customer extends Model
   protected $fillable = [
     'name', 'email', 'email_verified_at', 'password', 'remember_token'
   ];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
+
+
+  /**
+   * Get the identifier that will be stored in the subject claim of the JWT.
+   *
+   * @return mixed
+   */
+  public function getJWTIdentifier()
+  {
+    return $this->getKey();
+  }
+
+  /**
+   * Return a key value array, containing any custom claims to be added to the JWT.
+   *
+   * @return array
+   */
+  public function getJWTCustomClaims()
+  {
+    return [];
+  }
 
   /**
    * createdAt
